3901ecc32086f02d3f669bdd0c75eb03f97e2019
catfield123 <gtagta65@gmail.com>
2024-07-07 03:26:09 +0300

    actualized rabbitmq connection

--- src/mq.py
+++ src/mq.py
@@ -3,12 +3,12 @@ from config import RABBITMQ_AUTH_USER, RABBITMQ_AUTH_PASS
 
 credentials = pika.PlainCredentials(RABBITMQ_AUTH_USER, RABBITMQ_AUTH_PASS)
 rabbitmq_params = pika.ConnectionParameters(host='biokeeper_mq', port=5672,
-                                            virtual_host='microservices',
+                                            virtual_host='basic_vhost',
                                             credentials=credentials)
 
 
 def publish_user_created(id: int, username: str):
-    exchange_name = 'user.created'
+    exchange_name = 'users.topic'
     connection = pika.BlockingConnection(rabbitmq_params)
     channel = connection.channel()
     channel.basic_publish(exchange=exchange_name,
